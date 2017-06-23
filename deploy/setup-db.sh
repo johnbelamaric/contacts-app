a7d7a96c22c07f49609952e4df4394d23393490c
John Belamaric <jbelamaric@infoblox.com>
2017-06-23 18:09:40 -0400

    Rework setup db script

---
+++ deploy/setup-db.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+kubectl delete ns db 2>/dev/null
+while kubectl get ns 2>/dev/null db
+do
+	echo "Waiting for delete to finish..."
+	sleep 5
+done
+
+echo "Creating database..."
+
+kubectl apply -f manifest-db.yaml
+
+STATUS=$(kubectl -n db get pods -o jsonpath={.items[0].status.phase})
+while [ "$STATUS" != "Running" ]
+do
+	echo "Waiting for the database server container to come up ($STATUS)..."
+	sleep 5
+	STATUS=$(kubectl -n db get pods -o jsonpath={.items[0].status.phase})
+done
+
+echo "Giving the database time to initialize ($STATUS)..."
+
+sleep 5
+
+echo "Configuring database..."
+
+kubectl run mysql -i --rm --image=mysql --restart=Never -- mysql -hmysql.db -uroot -proot -e "create database contacts"
+kubectl run mysql -i --rm --image=mysql --restart=Never -- mysql -hmysql.db -uroot -proot -e "grant all on contacts.* to 'api'@'%' identified by 'mypassword'"
