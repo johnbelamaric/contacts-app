17ead1f921d8196bb96aa17cd8fcabf1b1406ef0
John Belamaric <jbelamaric@infoblox.com>
2017-08-02 11:34:37 -0400

    * Changes needed for OAuth2, plus some setup-db.sh improvements

--- deploy/gencerts.sh
+++ deploy/gencerts.sh
@@ -4,9 +4,14 @@
 #openssl req -x509 -new -nodes -key ca-key.pem -sha256 -days 5000 -out ca.crt -subj "/CN=ngp-primer-ca"
 
 #openssl genrsa -out contacts-api-key.pem 2048
-openssl req -new -key contacts-api-key.pem -out contacts-api.csr -subj "/CN=contacts-api" -config contacts-api.cnf
-openssl x509 -req -in contacts-api.csr -CA ca.crt -CAkey ca-key.pem -CAcreateserial -out contacts-api.pem -days 5000 -extensions v3_req -extfile contacts-api.cnf
+#openssl req -new -key contacts-api-key.pem -out contacts-api.csr -subj "/CN=contacts-api" -config contacts-api.cnf
+#openssl x509 -req -in contacts-api.csr -CA ca.crt -CAkey ca-key.pem -CAcreateserial -out contacts-api.pem -days 5000 -extensions v3_req -extfile contacts-api.cnf
 
 #openssl genrsa -out minikube-key.pem 2048
 #openssl req -new -key minikube-key.pem -out minikube.csr -subj "/CN=minikube" -config minikube.cnf
 #openssl x509 -req -in minikube.csr -CA ca.crt -CAkey ca-key.pem -CAcreateserial -out minikube.pem -days 5000 -extensions v3_req -extfile minikube.cnf
+
+openssl genrsa -out oauth2-key.pem 2048
+openssl req -new -key oauth2-key.pem -out oauth2.csr -subj "/CN=oauth2-proxy" -config oauth2.cnf
+openssl x509 -req -in oauth2.csr -CA ca.crt -CAkey ca-key.pem -CAcreateserial -out oauth2.pem -days 5000 -extensions v3_req -extfile oauth2.cnf
+
--- deploy/setup-db.sh
+++ deploy/setup-db.sh
@@ -3,27 +3,40 @@
 kubectl delete ns db 2>/dev/null
 while kubectl get ns 2>/dev/null db
 do
-	echo "Waiting for delete to finish..."
+	echo "$(date): Waiting for delete to finish..."
 	sleep 5
 done
 
-echo "Creating database..."
+echo "$(date): Creating database..."
 
 kubectl apply -f manifest-db.yaml
 
 STATUS=$(kubectl -n db get pods -o jsonpath={.items[0].status.phase})
 while [ "$STATUS" != "Running" ]
 do
-	echo "Waiting for the database server container to come up ($STATUS)..."
+	echo -n "$(date): Waiting for the database server container to come up ($STATUS)..."
+	if [ -z "$REASON" ]; then
+		echo
+	else
+		echo $REASON
+	fi
 	sleep 5
-	STATUS=$(kubectl -n db get pods -o jsonpath={.items[0].status.phase})
+	INFO=$(kubectl -n db get pods -o jsonpath='{.items[0].status.phase},{.items[0].status.containerStatuses[0].state.waiting.reason}')
+	STATUS=$(echo $INFO | cut -d , -f 1)
+	REASON=$(echo $INFO | cut -d , -f 2)
 done
 
-echo "Giving the database time to initialize ($STATUS)..."
+echo $(date): Giving the database time to initialize ($STATUS)...
 
-sleep 10
+READY="no"
+while [ "$READY" != "ready" ]
+do
+	READY=$(kubectl run mysql -i --rm --image=mysql --restart=Never -- mysql -hmysql.db -uroot -proot -e "select 'ready'" | grep ready | uniq)
+	echo "$(date): Waiting for the database to be initialized..."
+	sleep 5
+done
 
-echo "Configuring database..."
+echo "$(date): Configuring database..."
 
 kubectl run mysql -i --rm --image=mysql --restart=Never -- mysql -hmysql.db -uroot -proot -e "create database contacts"
 kubectl run mysql -i --rm --image=mysql --restart=Never -- mysql -hmysql.db -uroot -proot -e "grant all on contacts.* to 'api'@'%' identified by 'mypassword'"
